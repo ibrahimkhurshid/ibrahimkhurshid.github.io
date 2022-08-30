bf6b459c58c4937cccc1a572b53f881f613bf3c7
Ibrahim Khurshid <ibrahimkhurshid@hotmail.com>
2022-08-30 16:28:59 +0500

    updated technologies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Ibrahim Khurshid 
-        </a>
-      </header>
+      <h1>Ibrahim Khurshid</h1>
+      <img src="https://picsum.photos/300"></img>
+      <div>Technology</div>
+      <span>ReactJs</span>
+      <span>NestJs</span>
     </div>
   );
 }
